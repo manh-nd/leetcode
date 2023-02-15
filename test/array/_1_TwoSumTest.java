89bbb919cb5ae068e622d4f3a767c25595fb5276
manhnd <manhnd.695@gmail.com>
2023-02-16 04:27:58 +0700

    test: add another test data

--- test/array/_1_TwoSumTest.java
+++ test/array/_1_TwoSumTest.java
@@ -12,7 +12,8 @@ public class _1_TwoSumTest {
         return Stream.of(
                 Arguments.of(new int[]{2, 7, 11, 15}, 9, new int[]{0, 1}),
                 Arguments.of(new int[]{3, 2, 4}, 6, new int[]{1, 2}),
-                Arguments.of(new int[]{3, 3}, 6, new int[]{0, 1})
+                Arguments.of(new int[]{3, 3}, 6, new int[]{0, 1}),
+                Arguments.of(new int[]{3, 2}, 9, new int[]{})
         );
     }
     @ParameterizedTest(name = "input {0}, target {1}, expected {2}")
