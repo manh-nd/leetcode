21434ac0f987ee2292ec2bbf514dd8fe962a8d0c
manhnd <manhnd.695@gmail.com>
2023-02-16 13:14:18 +0700

    refactor: split into 2 while loops

--- src/array/_88_MergeSortedArray.java
+++ src/array/_88_MergeSortedArray.java
@@ -5,30 +5,25 @@ package array;
  */
 public class _88_MergeSortedArray {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m + n;
+        int i = m + n - 1;
 
-        while(i >= 0) {
-            if(m > 0 && n > 0) {
-                int num1 = nums1[m - 1];
-                int num2 = nums2[n - 1];
+        while(m > 0 && n > 0) {
+            int num1 = nums1[m - 1];
+            int num2 = nums2[n - 1];
 
-                if(num1 > num2) {
-                    nums1[i - 1] = num1;
-                    m--;
-                } else {
-                    nums1[i - 1] = num2;
-                    n--;
-                }
-            } else if(m > 0) {
-                int num = nums1[m - 1];
-                nums1[i - 1] = num;
+            if(num1 > num2) {
+                nums1[i--] = num1;
                 m--;
-            } else if(n > 0) {
-                int num = nums2[n - 1];
-                nums1[i - 1] = num;
+            } else {
+                nums1[i--] = num2;
                 n--;
             }
-            i--;
+        }
+
+        while(n > 0) {
+            int num = nums2[n - 1];
+            nums1[i--] = num;
+            n--;
         }
     }
 }
